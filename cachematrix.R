37a4f9514145ee68a29992f98bd9824512d6ed4a
4makrim <4makrim@users.noreply.github.com>
2014-12-20 20:04:01 -0800

    Update cachematrix.R
    Completed makeCacheMatrix() and cacheSolve() functions to complete assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Run the following commands to check if the functions are working.
+#source: cachematrix.R
+#jx <- makeCacheMatrix(x)
+#jx$set(x)
+#cacheSolve(jx)
+# NOTE: x is the matrix that is to be inversed. 
+# use cbind, rbind or matrix to create a square matrix that needs to be inverted.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a list of functions to operate on a matrix.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(xm = matrix()) {
+  set <- function(ym) {
+    xm <<- ym
+    im <<- NULL
+  }
+  get <- function() xm
+  setinverse <- function(inverse) im <<- inverse
+  getinverse <- function() im
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## returns the inverse if it is already calcuated and present in the environment.
+## else runs the solve() command to get the inverse of the matrix passed as argument.
+cacheSolve <- function(xm, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  im <- xm$getinverse()
+  if(!is.null(im)) {
+    message("getting cached data")
+    return(im)
+  }
+  data <- xm$get()
+  
+  #im <- solve(data)
+  im <- tryCatch(solve(data), error = function(e) e)
+  if (any(class(im) == "error")) {
+    print("Use a invertible matrix");
+    xm$setinverse(NULL)
+  } else {
+    xm$setinverse(im)
+  }
+  im
 }
